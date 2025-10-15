0750776b9f12481dcae124aedfc27832eccdaabd
mirerakepha <kephamirera16@gmail.com>
2025-10-15 18:15:30 +0300

    add models for auth

---
+++ src/dtos.rs
@@ -0,0 +1,144 @@
+//data exchange object structure
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use utoipa::{IntoParams, ToSchema};
+use validator::Validate;
+use crate::models::User;
+
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize, ToSchema)]
+
+
+//=============== REGISTER ===============
+pub struct RegisterUserDto {
+
+    #[Validate(length(min = 1, message = "name is required"))]
+    pub name: String, 
+
+    #[validate(
+        length(min = 1, message = "Email is required")
+        email(message = "Email is invalid")
+    )]
+    pub email: String,
+
+    #[Validate(
+        length(min = 1, message = "Password is Required")
+        length(min = 6, message = "Password must be at least 6 characters")
+    )]
+    pub password: String,
+
+    #[Validate(
+        length(min = 1, message = "Confirm Password is required"),
+        must_match(other = "password", message = "passwords do not match")
+    )]
+    #[serde(rename = "passwordConfirm")]
+    pub password_confirm: String,
+}
+
+
+
+//================= LOGIN ==================
+
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize, ToSchema)]
+pub struct LoginUserDto {
+
+    #[validate(length(min = 1, message = "Email is required"), email(message = "Email is valid"))]
+    pub email: String,
+
+    #[validate(
+        length(min = 1, message = "Password is required"),
+        length(min = 6, message = "Password must be at least 6 characters")
+    )]
+    pub password: String,
+
+}
+
+
+
+#[derive(Serialize, Deserialize, Validate, IntoParams)]
+pub struct RequestQueryDto {
+    #[validate(range(min = 1))]
+    pub page: Option<usize>,
+
+    #[validate(range(min = 1, max = 50))]
+    pub limit: Option<usize>,
+}
+
+
+
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct FilterUserDto {
+    pub id: String,
+    pub name: String,
+    pub email: String,
+    pub role: String,
+    pub photo: String,
+    pub verified: bool,
+    #[serde(rename = "createdAt")]
+    pub created_at: DateTime<Utc>,
+    #[serde(rename = "updatedAt")]
+    pub updated_at: DateTime<Utc>,
+}
+impl FilterUserDto {
+    pub fn filter_user(user: &User) -> Self {
+        FilterUserDto {
+            id: user.id.to_string(),
+            name: user.name.to_owned(),
+            email: user.email.to_owned(),
+            photo: user.photo.to_owned(),
+            verified: user.verified,
+            role: user.role.to_str().to_string(),
+            created_at: user.created_at.unwrap(),
+            updated_at: user.updated_at.unwrap(),
+        }
+    }
+    pub fn filter_user(user: &[User]) -> Vec<FilterUserDto>{
+        user.iter().map(FilterUserDto::filter_user).collect()
+    }
+}
+
+
+
+#[derive(Debug, Serialize, Deserialize. ToSchema)]
+pub struct UserData {
+    pub user: FilterUserDto,
+}
+
+
+
+
+#[derive(Debug, Serialize, Deserialize. ToSchema)]
+pub struct UserResponseDto {
+    pub status: String,
+    pub data: UserData,
+}
+
+
+
+
+#[derive(Debug, Serialize, Deserialize. ToSchema)]
+pub struct UserListResponseDto {
+    pub status: String,
+    pub users: Vec<FilterUserDto>,
+    pub result: usize,
+}
+
+
+
+#[derive(Debug, Serialize, Deserialize. ToSchema)]
+pub struct UserLoginResponseDto {
+    pub status: String,
+    pub token: String,
+}
+
+
+
+
+
+#[derive(Serialize, Deserialize. ToSchema)]
+pub struct Response {
+    pub status: &'static str,
+    pub message: String,
+}
+
---
+++ src/models.rs
@@ -0,0 +1,41 @@
+use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserealize, Serialize, Clone, Copy, sqlx::Type, PartialEq)]
+#[sqlx(type_name = "user_role", rename-all = "lowercase")]
+
+
+pub enum UserRole {
+    Admin,
+    Moderator,
+    User
+}
+
+impl UserRole {
+    pub fn to_str(&self) -> &str {
+        match self {
+            UserRole::Admin => "admin",
+            UserRole::Moderator => "moderator",
+            UserRole::User => "user"
+        }
+    }
+}
+
+#[derive(Debug, Deserealize, Serialize, sqlx::FromRow, sqlx::Type, Clone)]
+pub struct User {
+    pub id: uuid::Uuid,
+    pub name: String, 
+    pub email: String, 
+    pub password: String,
+    pub role: UserRole,
+    pub photo: String,
+    pub verified: bool,
+    #[serde(rename = "createdAt")]
+    pub created_at: Option<DateTime<Utc>>,
+    #[serde(rename = "updatedAt")]
+    pub updated_at: Option<DateTime<Utc>>
+}
+
+
+
+
